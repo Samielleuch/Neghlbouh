491a0761802549b1ae3f824a0a4a0da2cd16a834
Samielleuch <Sami.Elleuch@insat.u-carthage.tn>
2020-03-28 17:41:08 +0100

    Setting up Develop branch

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <br>
+    <h1> this is the Develop branch !</h1> <br/>
+    <strong>
+      branch into a feature branch with the number of your User Story
+    </strong>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
