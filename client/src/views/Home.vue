d0e6ad596a060ede0acad1dfc450ea9fea3383c8
CallmeMehdi <mehdi.mabrouki@yahoo.com>
2020-03-30 04:08:15 +0100

    Steps

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
     <MainBanner />
+    <Steps />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import MainBanner from "@/components/MainBanner.vue";
-
+import Steps from "@/components/Steps.vue";
 export default {
   name: "Home",
   components: {
-    MainBanner
+    MainBanner,
+    Steps
   }
 };
 </script>
