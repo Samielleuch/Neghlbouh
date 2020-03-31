9db4bd77e66107642485a01d72b9df3c1b22abdd
zakaria naaija <zakarianaaija@gmail.com>
2020-03-31 16:49:34 +0100

    commit message here

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   db: { dbName: "Cluster0", dbUser: "aymenletifi", dbPassword: "azertyuiop1" },
   hostname: "localhost",
-  port: process.env.PORT || 8081
+  port: process.env.PORT || 8081,
+  secretKey: '12345-67890-09876-54321'
 };
