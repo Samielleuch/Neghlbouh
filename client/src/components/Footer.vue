c30b3a04e8113fa73feb1811f8cee3e2d43565bd
amiradridi <dridiamira50@gmail.com>
2020-04-01 03:12:07 +0200

    header_footer

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-footer padless>
+    <v-card class="flex" flat tile color="black">
+      <v-card-text class="py-2 black--text text-center">
+        <strong style="color: white">2020 © جميع الحقوق محفوظة</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
