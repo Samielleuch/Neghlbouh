69e5d234701f8f4a60693609d89ca7dffa79f96c
Raed Ouerfelli <werfelli.raed@yahoo.fr>
2020-04-02 00:21:38 +0200

    initialized routes

--- server/src/models/Demande.js
+++ server/src/models/Demande.js
@@ -2,12 +2,17 @@ const mongoose = require('mongoose')
 
 const DemandeSchema = mongoose.Schema({
     cin: String,
-    firstName: String,
-    lastName: String,
     reason: String,
     date: {
         type: Date,
         default: Date.now
+    },
+    when: String,
+    duration : String,
+    where : String,
+    state: {
+        type:Number,
+        default: 0
     }
 })
 
--- server/src/routers/routes/demande.js
+++ server/src/routers/routes/demande.js
@@ -2,8 +2,17 @@ const express = require("express");
 const router = express.Router();
 const DemandeController = require('../../controllers/demande')
 
+//add "demande" to database
 router.post("/add", (req, res) => {
   DemandeController.addDemande(req, res);
 });
 
+// accept demande (state -> 1)
+router.put("/state/1",(req,res)=>{
+
+});
+
+router.put("/state/0",(req,res)=>{
+  
+});
 module.exports = router;
