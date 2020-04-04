7756a6993b83260c31aedbcb96dcb400ac7131c7
Samielleuch <Sami.Elleuch@insat.u-carthage.tn>
2020-04-04 04:35:42 +0100

    adding sign in

--- client/src/components/SignInForm.vue
+++ client/src/components/SignInForm.vue
@@ -1,113 +1,116 @@
 <template>
   <div>
-    <!-- exit button  -->
-    <v-row v-if="isModal">
-      <v-spacer></v-spacer>
-      <v-btn icon @click="pressLogin(false)" class=" mt-2 ml-5">
-        <v-icon>far fa-times-circle</v-icon>
-      </v-btn>
-    </v-row>
-    <!---->
-    <v-row>
-      <v-col cols="12" align="center">
-        <span class="text-center font-login  ">
-          {{ text.popup_text1 }}
-        </span>
-      </v-col>
-    </v-row>
-    <!-- icon -->
-    <v-row justify="center">
-      <v-col cols="12" align="center">
-        <v-img src="../assets/logo.png" width="400" aspect-ratio="3"></v-img>
-      </v-col>
-    </v-row>
-    <!---->
-    <v-row align="center">
-      <v-col cols="12" align="center">
-        <span class="text-center  font-login-popup2 ">
-          {{ text.popup_text2 }}
-        </span>
-      </v-col>
-    </v-row>
-    <v-card-text>
-      <v-form v-model="valid" ref="form">
-        <!-- CIN  -->
-        <v-row class="mb-0 pb-0">
-          <v-col cols="12" class="mb-0 pb-0">
-            <v-text-field
-              v-model="CIN"
-              outlined
-              rounded
-              prepend-inner-icon="far fa-address-card"
-              filled
-              clearable
-              :label="text.cinField"
-              required
-              color="black"
-              class="mb-0 pb-0"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <!-- Password  -->
-        <v-row class="mt--5">
-          <v-col cols="12">
-            <v-text-field
-              v-model="password"
-              :append-icon="show1 ? 'fas fa-eye' : 'fas fa-eye-slash'"
-              :type="show1 ? 'text' : 'password'"
-              :label="text.passWordField"
-              rounded
-              outlined
-              prepend-inner-icon="fas fa-lock"
-              clearable
-              filled
-              @click:append="show1 = !show1"
-              required
-              color="black"
-              class="mt-0 pb-0"
-            >
-            </v-text-field>
-          </v-col>
-        </v-row>
-      </v-form>
-      <!-- submit Button  -->
+    <v-form ref="form" v-model="valid">
+      <!-- exit button  -->
+      <v-row v-if="isModal">
+        <v-spacer></v-spacer>
+        <v-btn icon @click="pressLogin(false)" class=" mt-2 ml-5">
+          <v-icon>far fa-times-circle</v-icon>
+        </v-btn>
+      </v-row>
+      <!---->
       <v-row>
         <v-col cols="12" align="center">
-          <v-hover v-slot:default="{ hover }">
-            <!-- when the button gets hovered hover becomes true so we switch the class -->
-            <v-btn
-              :class="hover ? 'mt--10 glowing-border' : 'mt--10'"
-              :disabled="!valid"
-              color="#df0100"
-              @click="validate"
-              rounded
-              :ripple="{ class: 'red--text' }"
-              height="225%"
-              width="100%"
-            >
-              <span class="font-login-white">
-                {{ text.submitField }}
-              </span>
-            </v-btn>
-          </v-hover>
+          <span class="text-center font-login  ">
+            {{ text.popup_text1 }}
+          </span>
         </v-col>
       </v-row>
-      <!-- Forgot Password section -->
+      <!-- icon -->
       <v-row justify="center">
-        <v-col cols="9" align="center">
-          <em>
-            {{ text.forgotPass }}
-          </em>
-          <a class=" font-weight-bold font-login-pass" href="#">
-            {{ text.makeAccount }}
-          </a>
+        <v-col cols="12" align="center">
+          <v-img src="../assets/logo.png" width="400" aspect-ratio="3"></v-img>
         </v-col>
       </v-row>
-    </v-card-text>
+      <!---->
+      <v-row align="center">
+        <v-col cols="12" align="center">
+          <span class="text-center  font-login-popup2 ">
+            {{ text.popup_text2 }}
+          </span>
+        </v-col>
+      </v-row>
+      <v-card-text>
+        <v-form v-model="valid" ref="form">
+          <!-- CIN  -->
+          <v-row class="mb-0 pb-0">
+            <v-col cols="12" class="mb-0 pb-0">
+              <v-text-field
+                v-model="CIN"
+                outlined
+                rounded
+                prepend-inner-icon="far fa-address-card"
+                filled
+                clearable
+                :label="text.cinField"
+                required
+                color="black"
+                class="mb-0 pb-0"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <!-- Password  -->
+          <v-row class="mt--5">
+            <v-col cols="12">
+              <v-text-field
+                v-model="password"
+                :append-icon="show1 ? 'fas fa-eye' : 'fas fa-eye-slash'"
+                :type="show1 ? 'text' : 'password'"
+                :label="text.passWordField"
+                rounded
+                outlined
+                prepend-inner-icon="fas fa-lock"
+                clearable
+                filled
+                @click:append="show1 = !show1"
+                required
+                color="black"
+                class="mt-0 pb-0"
+              >
+              </v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
+        <!-- submit Button  -->
+        <v-row>
+          <v-col cols="12" align="center">
+            <v-hover v-slot:default="{ hover }">
+              <!-- when the button gets hovered hover becomes true so we switch the class -->
+              <v-btn
+                :class="hover ? 'mt--10 glowing-border' : 'mt--10'"
+                :disabled="!valid"
+                color="#df0100"
+                @click="validate"
+                rounded
+                :ripple="{ class: 'red--text' }"
+                height="225%"
+                width="100%"
+              >
+                <span class="font-login-white">
+                  {{ text.submitField }}
+                </span>
+              </v-btn>
+            </v-hover>
+          </v-col>
+        </v-row>
+        <!-- Forgot Password section -->
+        <v-row justify="center">
+          <v-col cols="9" align="center">
+            <em>
+              {{ text.forgotPass }}
+            </em>
+            <a class=" font-weight-bold font-login-pass" href="#">
+              {{ text.makeAccount }}
+            </a>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-form>
   </div>
 </template>
 
 <script>
+import authController from "@/services/AuthenticationService";
 import { mapActions, mapState } from "vuex";
 export default {
   name: "SignInForm",
@@ -133,8 +136,22 @@ export default {
   },
   methods: {
     ...mapActions(["pressLogin"]),
-    validate() {
-      this.valide = true;
+    async validate() {
+      this.$refs.form.validate();
+      if (this.valid) {
+        this.loading = true;
+        let resp = await authController.signIn({
+          cin: this.CIN,
+          password: this.password
+        });
+        this.loading = false;
+        console.log(resp);
+        this.pressLogin(false);
+        this.$router.push({ name: "Home" });
+      } else {
+        //to implement notification v-if here
+        console.log("validation failed");
+      }
     }
   }
 };
