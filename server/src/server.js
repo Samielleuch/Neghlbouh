2e7c7c27fc72853ca5cf4ce8534b8b6217958d8c
zakaria naaija <zakarianaaija@gmail.com>
2020-04-12 20:20:21 +0100

    braclet endpoint and reconfiguring server to support socket.io

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const config = require('./config/config')
 const logger = require("morgan");
 const passport = require('passport');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 
 //Middlewares
@@ -13,7 +15,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
-
+app.set('socketio', io);
+io.on('connection', (socket) => {
+  console.log('user connected!!');
+  socket.on('disconnect', () =>  {
+    io.emit('user disconnected');
+});
+});
 //our routes are located in /api/v1/{routefilename}
 const route = require('./routers/router')(app);
 mongoose.connect(
@@ -24,6 +32,7 @@ mongoose.connect(
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-app.listen(config.port, config.hostname, () => {
+server.listen(config.port, config.hostname, () => {
   console.log(`Server running at http://${config.hostname}:${config.port}/`);
 });
+
